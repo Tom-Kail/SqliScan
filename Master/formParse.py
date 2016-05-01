9e483a491829cb95746eb4e52d71991c1e45961e
CNT <72727266990@qq.com>
2016-05-01 16:07:57 +0800

    initial

---
+++ Master/formParse.py
@@ -0,0 +1,180 @@
+#coding=utf-8
+from bs4 import BeautifulSoup
+import lxml
+import urlparse
+from master import Request
+
+class Input(object):
+    def __init__(self,content):
+        try:
+            self._name = content.attrs['name']
+        except Exception as err:
+            self._name = ''
+
+        try:
+            self._type = content.attrs['type']
+        except Exception as err:
+            self._type = 'text'
+        
+        try:
+            self._value = content.attrs['value']        
+        except Exception as err:
+            self._value = 'cntvalue' #'cnt' are first characters of my PinYin name, I use it as default value of input tag
+        finally:
+            if self._value == '':
+                self._value = 'cntvalue'
+        try:
+            self._checked = content.attrs['checked']
+        except Exception as err:
+            self._checked = ''
+class TextArea(object):
+    def __init__(self,content):
+        try:
+            self._name = content.attrs['name']     
+        except Exception as err:
+            self._name = ''
+
+        try:
+            self._value = content.attrs['value']        
+        except Exception as err:
+            self._value = ''
+class Select(object):
+    def __init__(self,content):
+        #if select contains several options, use first option value as select defalult value
+        try:
+            self._name = content.attrs['name']
+        except Exception as err:
+            self._name = ''
+        if len(content.contents) != 0:
+            try:
+                self._value = content.contents[0].attrs['value']
+            except Exception as err:
+                self._value = 'cntvalue'
+        else:
+            self._value = 'cntvalue'
+class Form(object):
+    def __init__(self,base,content):
+        '''
+        example of 'content':
+           ' <form>
+                <input name="name">
+            </form>'
+        'scheme', 'netloc' and 'path' come form parent url
+        '''
+        # parse form content
+        soup = BeautifulSoup(content)
+        try:
+            self._method = soup.form.attrs['method'].lower()
+        except Exception as err:
+            self._method = 'get'
+
+        try:
+            self._action = soup.form.attrs['action']
+        except Exception as err:
+            self._action = ''
+        self._base = base
+        self._inputs = []
+        self._selects = []
+        self._textareas = []
+        for child in soup.find_all(name='input'):
+            try:
+                if child.attrs['name'] == '':
+                    continue
+            except Exception as err:
+                continue
+            self._inputs.append(Input(child))
+        for child in soup.find_all(name='select'):
+            try:
+                if child.attrs['name'] == '':
+                    continue
+            except Exception as err:
+                continue
+            self._selects.append(Select(child))
+
+        for child in soup.find_all(name='textarea'):
+            try:
+                if child.attrs['name'] == '':
+                    continue
+            except Exception as err:
+                continue
+            self._textareas.append(TextArea(child))
+
+        # generate queries from input, textarea and select
+        query = ''
+        payload = {}
+        checkboxCount = False
+        for child in self._inputs:
+            payload[child._name] = child._value
+            if child._type == 'radio' and child._checked == 'checked':    
+                query =query + child._name + '=' + child._value + '&'
+            elif child._type == 'checkbox' and checkboxCount == False:
+                query = query + child._name + '=' + child._value + '&'
+                checkboxCount = True
+            elif child._type == '' or child._type == 'text' or child._type == 'password':
+                query = query + child._name + '=' + child._value + '&'
+            else:
+                query = query + child._name + '=' + child._value + '&'
+       
+        for child in self._textareas:
+            payload[child._name] = child._value
+            query = query + child._name + '=' + child._value + '&'
+        
+        for child in self._selects:
+            payload[child._name] = child._value
+            query = query + child._name + '=' + child._value + '&'
+        # add query in action
+        actionTup = urlparse.urlparse(self._action)
+        query = query + actionTup.query
+        if len(query) > 0 and query[len(query)-1] == '&':
+            query = query[0:len(query)-1]
+        
+        for line in actionTup.query.strip().split('&'):
+            if line == '':
+                break
+            name,value = line.strip().split('=',1)
+            payload[name] = value 
+        # use action and query to generate tmpUrl
+        if self._method == 'get':
+            tmpUrl = actionTup.path + '?' + query
+            self._url = urlparse.urljoin(base,tmpUrl)
+            self._payload = {} 
+        elif self._method == 'post':
+            tmpUrl = actionTup.path
+            self._url = urlparse.urljoin(base,tmpUrl)
+            self._payload = payload
+        '''    
+        # dirPath of parent url
+        pos = path.rfind('/')
+        dirPath = ''
+        if pos != -1:
+            dirPath = path[0:pos+1]
+        else:
+            dirPath = '/'
+        tup = urlparse.urlparse(self._action)
+        tmpUrl = ''
+        # generate tmpUrl
+        if tup.path == '':
+            # current webpage
+            tmpUrl = scheme + '://' + netloc + path + '?'
+        elif tup.path.find('/') == -1:
+            # like 'file2.php?a=one'
+            tmpUrl = scheme + '://' + netloc + dirPath + tup.path + '?'
+        elif tup.path[0] == '/':
+            # like '/' or '/file.php?a=b'
+            tmpUrl = scheme + '://' + netloc +tup.path + '?'
+        else:
+            pass
+        # add query to tmpUrl
+        if tup.query != '':
+            tmpUrl = tmpUrl + tup.query + '&' + query
+        else:
+            tmpUrl = tmpUrl + query
+        '''           
+    def getUrl(self):
+        return self._url
+    def getReq(self):
+        return Request(self._base,self._url,self._method,self._payload,source='form')
+    def getMethod(self):
+        return self._method
+    def getPayload(self):
+        return self._payload
---
+++ Master/master.py
@@ -0,0 +1,175 @@
+# coding=utf-8
+import Queue
+import webbrowser
+import re
+import requests
+import bloomFilter
+import urlparse
+import formParse
+from bs4 import BeautifulSoup
+from posixpath import normpath
+
+class Request(object):
+    def __init__(self,base,url,method='get',payload={},source='regex'):
+        self._url = myJoin(base,url)
+        self._method = method
+        self._payload = payload
+        self._BFUrl = '' # like ID to identify unique request
+        self._source = source
+        tup = urlparse.urlparse(self._url)
+        
+        if self._method == 'get':
+            if tup.query != '':
+                self._BFUrl = urlparse.urlunparse((tup.scheme,tup.netloc,tup.path,tup.params,'','')) + '?'
+                for line in tup.query.strip().split('&'):
+                    if line.find('=') == -1:# in case of www.baidu.com/?www.google.com
+                        continue
+                    #print 'url: ',url,' line:',line,' pos: ',pos
+                    name, value = line.strip().split('=',1)
+                    self._BFUrl = self._BFUrl + name + '&'
+            else:
+                self._BFUrl = self._url
+        elif self._method == 'post': 
+            self._BFUrl = self._BFUrl + url + '?'
+            for k in payload.keys():
+                self._BFUrl = self._BFUrl + k + '&'
+def myJoin(base,url): 
+    tup = urlparse.urlparse(url)
+    #if tup.netloc == '' and url.find('/') != -1 and len(url) > 0 and url[0] != '/':
+    #    url = '/' + url
+        #print 'Special Url: ',url
+    rst = urlparse.urljoin(base,url)
+    #print '   base: ',base,' url: ',url,' rst:',rst
+    return rst
+'''
+    urlTmp = urlparse.urljoin(base,url)
+    arr = urlparse.urlparse(urlTmp)
+    path = normpath(arr[2])
+    
+    return urlparse.urlunparse((arr.scheme,arr.netloc,path,arr.params,arr.query,arr.fragment))
+'''
+def getCompleteUrl(scheme,netloc,path,tmpUrl):
+    dirPath = ''
+    pos = path.rfind('/')
+    if pos != -1:
+        dirPath = path[0:pos+1]
+    else:
+        dirPath = '/'
+    tup = urlparse.urlparse(tmpUrl)
+
+    # example:
+    #   url: 'example8.php?order=name'
+    #   parent url: '192.168.42.138/sqli/example8.php?order=age'
+    #   result: '192.168.42.138/sqli/example8.php?order=name'
+    if tup.netloc == '':
+        if tup.path =='':
+            tmpUrl = netloc + path + '?' + tup.query
+        elif tup.path[0] == '/':
+            tmpUrl = netloc + tmpUrl
+        elif tup.path.find('/') == -1:
+            tmpUrl = netloc + dirPath + tmpUrl
+        else:
+            tmpUrl = netloc + '/' + tmpUrl
+    if tup.scheme == '':
+        tmpUrl = scheme + '://' + tmpUrl
+    return tmpUrl
+
+
+
+if __name__ == "__main__":
+    #seed = 'http://192.168.42.138/' # web for pentest
+    #seed = Request(base='http://192.168.42.131/dvwa/index.php',url='http://192.168.42.131/dvwa/index.php',method='get')
+    seed = Request(base='http://192.168.42.138',url='http://192.168.42.138',method='get')
+    print 'seed url: ',seed._url
+    print 'Do you want to recording cookie?'
+    recordCookie = raw_input('(Y/N)')
+    if recordCookie.lower() == 'y':
+        # recording cookie
+        try:
+            webbrowser.open_new(seed._url)
+        except Exception as err:
+            print err
+        print 'Please enter "Y" after finishing recording cookie.'
+        while True: 
+            finish = raw_input('')
+            if finish.lower() == 'y':
+                break
+            else:
+                finish = ''
+    # read cookie from /root/cntzapfile.txt
+    cookieFileName = '/root/cntzapfile.txt'
+    print 'Read cookie from file: ',cookieFileName
+    cookiePat = re.compile(r'\bCookie:([\S \t]*)')
+    allText = open(cookieFileName).read()
+    cookieTmp = cookiePat.findall(allText)
+    cookies = {}
+    if len(cookieTmp) != 0:
+        for line in cookieTmp[0].split(';'):
+            name,value = line.strip().split('=',1)
+            cookies[name] = value
+    print 'cookie is:',cookies
+    # begin crawler
+    tup = urlparse.urlparse(seed._url)
+    netloc = tup.netloc # seed url 
+    count = 0
+    q = Queue.Queue()
+    q.put(seed)
+    #q.put(Request('http://192.168.42.131/dvwa/vulnerabilities/sqli/','http://192.168.42.131/dvwa/vulnerabilities/sqli/','get'))
+    bf = bloomFilter.BloomFilter(0.001,100000)
+    bf.insert(seed._url)
+    while(not q.empty()):
+        req = q.get()
+        print 'req._BFUrl: ',req._BFUrl,' ',req._method,' ', req._source
+        #print 'Url: ',req._url
+        count += 1
+        html = ''
+        try:
+            if req._method == 'get':
+                r = requests.get(req._url,timeout=1,cookies=cookies)
+                html = r.content
+            else:
+                r = requests.post(req._url,data=req._payload,timeout=1,cookies=cookies)
+                html = r.content
+        except Exception as err:
+            print '[Error]: ',err
+        
+        # parse form in response content
+        soup = BeautifulSoup(html)
+        formUrls = []
+        #formPat = re.compile(r'<form[\S\s]*?</form>')
+        #forms = formPat.findall(r.content)
+        forms = []
+        try:
+            forms = soup.find_all(name='form')
+        except Exception as err:
+            print err
+
+        #forms = soup.find_all(name='form')
+        for child in forms:
+            #print child
+            #tmp = BeautifulSoup(child)
+            tmpForm = formParse.Form(req._url,str(child))
+            formUrls.append(tmpForm.getReq())
+        #print 'form urls :',formUrls:
+        
+        hrefPat = re.compile(r'href="([\S]{5,})"')
+        srcPat = re.compile(r'src="([\S]{5,})"')
+        urls = hrefPat.findall(r.content)
+        urls.extend(srcPat.findall(r.content))
+        reqs =[]
+        reqs.extend(formUrls)
+        for url in urls:
+            if url.find('logout') != -1:
+                continue
+            tmpReq = Request(req._url,url,'get')
+            netlocTmp = urlparse.urlparse(tmpReq._url).netloc
+            if netlocTmp == netloc:
+                reqs.append(Request(req._url,url,'get'))
+        for x in reqs:
+            if not bf.exist(x._BFUrl):
+                bf.insert(x._BFUrl)
+                q.put(x)
+        
+    print "Number of url:",count
+
+            
